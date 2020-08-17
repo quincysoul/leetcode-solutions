8ab23a56f52086e80c660cd0e106cc077e321e6c
quincysoul <shawnit@live.com>
2020-08-16 18:42:28 -0700

    sudoku demo

---
+++ python_leetcode/solutions/template.py
@@ -0,0 +1,16 @@
+"""
+Summary:
+    Time Complexity: O(
+        * 
+    Space Complexity: O(
+Trick:
+
+Bibliography:
+
+Args:
+"""
+
+
+class Solution:
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        
